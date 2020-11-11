ec8da7ff68f9cc63fff118db66dfc1ba48b3bff2
momentchan <mingjyunhung@gmail.com>
2020-11-11 20:43:46 +0900

    create rendertexture to texture 2d converter

--- Assets/Common/RenderTextureUtil.cs
+++ Assets/Common/RenderTextureUtil.cs
@@ -15,6 +15,35 @@ namespace Common {
             return rt;
         }
 
+       public static Texture2D RenderTextureToTexture2D(RenderTexture rt) {
+            TextureFormat format;
+
+            switch (rt.format) {
+                case RenderTextureFormat.ARGBFloat:
+                    format = TextureFormat.RGBAFloat;
+                    break;
+                case RenderTextureFormat.ARGBHalf:
+                    format = TextureFormat.RGBAHalf;
+                    break;
+                case RenderTextureFormat.ARGBInt:
+                    format = TextureFormat.RGBA32;
+                    break;
+                case RenderTextureFormat.ARGB32:
+                    format = TextureFormat.ARGB32;
+                    break;
+                default:
+                    format = TextureFormat.ARGB32;
+                    Debug.LogWarning("Unsuported RenderTextureFormat.");
+                    break;
+            }
+
+            var tex2D = new Texture2D(rt.width, rt.height, format, false);
+            var rect = Rect.MinMaxRect(0f, 0f, tex2D.width, tex2D.height);
+            RenderTexture.active = rt;
+            tex2D.ReadPixels(rect, 0, 0);
+            RenderTexture.active = null;
+            return tex2D;
+        }
 
         public static bool IsResolutionChanged(RenderTexture rt, int width, int height) {
             if (rt.width != width || rt.height != height)
