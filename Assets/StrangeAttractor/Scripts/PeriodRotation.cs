f850afa10d531479b8cb583626f8877bb030d52e
momentchan <mingjyunhung@gmail.com>
2020-12-22 16:58:58 +0900

    add period rotation to strange attractor

---
+++ Assets/StrangeAttractor/Scripts/PeriodRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeriodRotation : MonoBehaviour
+{
+    [SerializeField] protected Vector3 rotateSpeed;
+    
+    void Update()
+    {
+        transform.Rotate(Vector3.right * rotateSpeed.x, Space.Self);
+        transform.Rotate(Vector3.up * rotateSpeed.y, Space.Self);
+        transform.Rotate(Vector3.forward * rotateSpeed.z, Space.Self);
+    }
+}
